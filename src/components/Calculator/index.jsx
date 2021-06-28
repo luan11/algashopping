f6261f3dd49b57b4a2f0e2e2ab4af127ab978182
Luan Novais <luaannovais@live.com>
2021-06-28 17:19:16 -0300

    Calculator component

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { connect } from 'react-redux';
+import { sum, subtract } from './../../store/Calculator/Calculator.actions';
 
-const Calculator = ({ result }) => {
+const Calculator = () => {
+  const dispatch = useDispatch();
+  const result = useSelector((state) => state.calculator);
+
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  function handleSum() {
+    dispatch(sum(a, b));
+  }
+
+  function handleSubtract() {
+    dispatch(subtract(a, b));
+  }
+
   return (
     <>
-      <input type="text" placeholder="A" />
-      <input type="text" placeholder="B" />
+      <input
+        type="text"
+        placeholder="A"
+        value={a}
+        onChange={(event) => setA(Number(event.target.value))}
+      />
+      <input
+        type="text"
+        placeholder="B"
+        value={b}
+        onChange={(event) => setB(Number(event.target.value))}
+      />
 
-      <button>Sum</button>
-      <button>Subtract</button>
+      <button type="button" onClick={handleSum}>
+        Sum
+      </button>
+
+      <button type="button" onClick={handleSubtract}>
+        Subtract
+      </button>
 
       <div>{result}</div>
     </>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    result: state.calculator,
-  };
-};
-
-export default connect(mapStateToProps)(Calculator);
+export default Calculator;
